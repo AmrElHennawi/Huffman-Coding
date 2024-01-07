cd6a5f544677e43e99757dc635c25ba060afc675
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-07 10:50:02 +0200

    Delete Main.java

--- Huffman/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Huffman h = new Huffman(//filePath);
-        h.compress();
-        h.decompress(//filePath);
-    }
-}
